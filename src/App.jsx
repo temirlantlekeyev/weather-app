32b8fa5b254ce37e5888c26e58aea7d69fd4a85b
Temirlan Tlekeyev <tlekeyev.temirlan@gmail.comm>
2024-09-20 23:17:02 +0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import Weather from './weather'
 
 function App() {
 
-  const apiKey = "aefd2f71e63c8a5170986fe5d1730612"
-  const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=almaty&appid=aefd2f71e63c8a5170986fe5d1730612"
- 
-
   return (
     <> 
     <Weather/>
--- src/weather.jsx
+++ src/weather.jsx
@@ -22,8 +22,7 @@ import axios from 'axios'
 
 const Weather = ()=> {
     
-const apiKey = "aefd2f71e63c8a5170986fe5d1730612"
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=almaty&appid=aefd2f71e63c8a5170986fe5d1730612"
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=almaty&appid=${import.meta.env.VITE_APP}`
  
   const icons = {
     "01d" : clearSkyd,
